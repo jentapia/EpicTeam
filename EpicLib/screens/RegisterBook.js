481c965ecb122610e0811a42e9cd7bc2cd8b07bd
danilopincheira <danilopincheira@users.noreply.github.com>
2021-04-23 14:29:10 +0100

    comments
    i added some comments

--- EpicLib/screens/RegisterBook.js
+++ EpicLib/screens/RegisterBook.js
@@ -2,14 +2,14 @@ import React, { useState } from 'react';
 import { View, ScrollView, KeyboardAvoidingView, Alert, SafeAreaView,Text, Button, TextInput} from 'react-native';
 import Mytextinput from './components/Mytextinput';
 import Mybutton from './components/Mybutton';
-
+//import the library to use sqlite
 import * as SQLite from 'expo-sqlite';
-
+//open the database
 const db = SQLite.openDatabase('db.db');
 
-
+//main function with navigation in the curly brackes to redirect to Home page.
 export default function RegisterBook({navigation}) {
-
+  // create for variables and we use useState to render the page
     let [book_id, setBook_id] = useState('');
     let [book_name, setBook_name] = useState('');
     let [author, setAuthor] = useState('');
@@ -17,7 +17,7 @@ export default function RegisterBook({navigation}) {
   
     let register_book = () => {
       console.log(book_id, book_name, author, cathegory);
-  
+   //check there is something to fill the imputs.
       if (!book_id) {
         alert('Please fill book id');
         return;
@@ -34,7 +34,8 @@ export default function RegisterBook({navigation}) {
         alert('Please fill Cathegory');
         return;
       }
-
+ 
+      //insert the values in the data base.
       db.transaction(function (tx) {
         tx.executeSql(
           'INSERT INTO table_books (book_id, book_name, author, cathegory) VALUES (?,?,?,?)',
@@ -43,6 +44,7 @@ export default function RegisterBook({navigation}) {
             console.log('Results', results.rowsAffected);
             if (results.rowsAffected > 0) {
               alert(
+                //gives an alert if the user is registered successful
                 'Success',
                 'Your book is Registered Successfully',
                 [
@@ -53,12 +55,14 @@ export default function RegisterBook({navigation}) {
                 ],
                 { cancelable: false }
               );
+              //gives an alert if there is something wrong with the register process
             } else alert('Registration Failed');
           }
         );
       });
     };
   
+    //we receive the values here passed with the props inside of MyTextInput and set the values
     return (
       <SafeAreaView style={{ flex: 1 }}>
         <View style={{ flex: 1, backgroundColor: 'white' }}>
@@ -67,13 +71,14 @@ export default function RegisterBook({navigation}) {
               <KeyboardAvoidingView
                 behavior="padding"
                 style={{ flex: 1, justifyContent: 'space-between' }}>
+                  
                 <Mytextinput
                   placeholder="Enter id of the book"
                   onChangeText={
                     (book_id) => setBook_id(book_id)
                   }
                   style={{ padding: 10 }}
-                />
+                /> 
                 <Mytextinput
                   placeholder="Enter the name of the book"
                   onChangeText={
