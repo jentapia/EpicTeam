7db9f9698c28f9e64c043a92e2e53d06b98977b8
jentapia <jentapia@gmail.com>
2021-04-27 00:49:06 +0100

    Update RegisterNewUser
    The role input was changed to a select option (picker)

--- EpicLib/screens/RegisterNewUser.js
+++ EpicLib/screens/RegisterNewUser.js
@@ -3,9 +3,10 @@
  * Import of the components
  */
 import React, { useState } from 'react';
-import { View, ScrollView, KeyboardAvoidingView, Alert, SafeAreaView,Text, Button, TextInput} from 'react-native';
+import { View, ScrollView, KeyboardAvoidingView, StyleSheet, SafeAreaView,Text, Button, TextInput} from 'react-native';
 import Mytextinput from './components/Mytextinput';
 import Mybutton from './components/Mybutton';
+import {Picker} from '@react-native-picker/picker';
 //Import from the library
 import * as SQLite from 'expo-sqlite';
 //variable used to open database
@@ -32,7 +33,7 @@ export default function RegisterNewUser({navigation}) {
         return;
       }
       if (!role) {
-        alert('Please fill Cathegory');
+        alert('Please fill Role');
         return;
       }
 
@@ -90,13 +91,18 @@ export default function RegisterNewUser({navigation}) {
                   //numberOfLines={30}
                   style={{ textAlignVertical: 'top', padding: 10 }}
                 />
-                <Mytextinput
-                  placeholder="Enter your Role"
-                  onChangeText={
-                    (role) => setRole(role)
-                  }
-                  style={{ padding: 10 }}
-                />
+                
+                <View style={styles.viewPick}>
+                  <Picker
+                    selectedValue={role}
+                    onValueChange={(itemValue, itemIndex) =>
+                      setRole(itemValue)
+                    } style={{ padding: 10, color: '#007FFF', borderColor: 'white'}}>
+                    <Picker.Item label="Select a Role" value=" " />
+                    <Picker.Item label="User" value="user" />
+                    <Picker.Item label="Admin" value="admin" />
+                  </Picker>
+                </View>
 
                 <Mybutton title="Submit" customClick={register_NewUser} />
             
@@ -122,3 +128,12 @@ export default function RegisterNewUser({navigation}) {
       </SafeAreaView>
     );
   };
+  const styles = StyleSheet.create({
+    viewPick: {
+      marginLeft: 35,
+      marginRight: 35,
+      marginTop: 10,
+      borderColor: '#007FFF',
+      borderWidth: 1,
+    }
+  });
